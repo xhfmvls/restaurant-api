7ed4268d4bb204600e2113d2b072ceaecc22a897
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-08-26 01:37:02 +0700

    CRUDMenu

--- pkg/controllers/menu-controller.go
+++ pkg/controllers/menu-controller.go
@@ -1,8 +1,11 @@
 package controllers
 
-import(
-	"net/http"
+import (
 	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 	"github.com/xhfmvls/restaurant-api/pkg/models"
 	"github.com/xhfmvls/restaurant-api/pkg/utils"
 )
@@ -28,13 +31,46 @@ func GetMenu(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetFood(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+	foodId := vars["foodId"]
+	id, err := strconv.ParseInt(foodId, 0, 0)
+	if err != nil {
+		panic("ID not valid")
+	}
+	foodDetails := models.GetFoodById(id)
+	res, _ := json.Marshal(foodDetails)
+	w.Header().Set("content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func DeleteFood(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+	foodId := vars["foodId"]
+	id, err := strconv.ParseInt(foodId, 0, 0)
+	if err != nil {
+		panic("ID not valid")
+	}
+	deletedFood := models.DeleteFoodById(id)
+	res, _ := json.Marshal(deletedFood)
+	w.Header().Set("content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func UpdateFood(w http.ResponseWriter, r *http.Request) {
-	
+	updateFood := &models.Food{}
+	utils.ParseBody(r, updateFood)
+
+	vars := mux.Vars(r)
+	foodId := vars["foodId"]
+	id, err := strconv.ParseInt(foodId, 0, 0)
+	if err != nil {
+		panic("ID not valid")
+	}
+	newFoodDetail := models.UpdateFoodById(updateFood, id)
+	res, _ := json.Marshal(newFoodDetail)
+	w.Header().Set("content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- pkg/models/menu-model.go
+++ pkg/models/menu-model.go
@@ -19,14 +19,38 @@ func init() {
 	db.AutoMigrate(&Food{})
 }
 
-func (f *Food) AddFood() *Food {
-	db.NewRecord(f)
-	db.Create(&f)
-	return f
+func (food *Food) AddFood() *Food {
+	db.NewRecord(food)
+	db.Create(&food)
+	return food
 }
 
 func GetMenu() []Food {
 	var Foods []Food
 	db.Find(&Foods)
 	return Foods
+}
+
+func GetFoodById(id int64) *Food {
+	var searchedFood Food
+	db.Where("ID=?", id).Find(&searchedFood)
+	return &searchedFood
+}
+
+func DeleteFoodById(id int64) *Food {
+	var deletedFood Food
+	db.Where("ID=?", id).Delete(&deletedFood)
+	return &deletedFood
+}
+
+func UpdateFoodById(food *Food, id int64) *Food {
+	foodDetails := GetFoodById(id)
+	if food.Name != "" {
+		foodDetails.Name = food.Name
+	}
+	if food.Price != 0 {
+		foodDetails.Price = food.Price
+	}
+	db.Save(&foodDetails)
+	return foodDetails
 }
