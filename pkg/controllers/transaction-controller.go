329a1c6e8f48d27a77dbe26d59aac640d1131885
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-09-02 04:02:44 +0700

    transaction functionality

---
+++ pkg/controllers/transaction-controller.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/xhfmvls/restaurant-api/pkg/middlewares"
+	"github.com/xhfmvls/restaurant-api/pkg/models"
+)
+
+func CreateTransaction(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(middlewares.IdKey).(int)
+	transaction := models.CreateTransaction(userId)
+	res, _ := json.Marshal(transaction)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
+}
+
+func GetUserTransactions(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(middlewares.IdKey).(int)
+	transactionsList := models.GetTransactionsHeaderList(userId)
+	res, _ := json.Marshal(transactionsList)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetUserTransactionDetail(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	transactionId, err := strconv.Atoi(vars["transactionId"])
+	if err != nil {
+		panic("ID invalid")
+	}
+	transactionInfo := models.GetTransactionDetails(transactionId)
+	res, _ := json.Marshal(transactionInfo)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
--- pkg/models/transaction-model.go
+++ pkg/models/transaction-model.go
@@ -1,4 +1,4 @@
-package models 
+package models
 
 import (
 	"github.com/jinzhu/gorm"
@@ -7,19 +7,19 @@ import (
 
 type TransactionHeader struct {
 	gorm.Model
-	UserId int `json:"userid"`
+	UserId         int  `json:"userid"`
 	PurchaseStatus bool `json:"purchasestatus"`
 }
 
 type TransactionDetail struct {
 	gorm.Model
 	TransactionId int `json:"transactionid"`
-	FoodId int `json:"foodid"`
-	Quantity int `json:"quantity"`
+	FoodId        int `json:"foodid"`
+	Quantity      int `json:"quantity"`
 }
 
 type TransactionInformation struct {
-	TrHeader TransactionHeader
+	TrHeader  TransactionHeader
 	TrDetails []TransactionDetail
 }
 
@@ -30,16 +30,57 @@ func init() {
 	db.AutoMigrate(&TransactionDetail{})
 }
 
-func (header *TransactionHeader)CreateTransactionHeader() *TransactionHeader {
+func (header *TransactionHeader) CreateTransactionHeader() *TransactionHeader {
 	db.NewRecord(header)
 	db.Create(&header)
 	return header
 }
 
-func (detail *TransactionDetail)CreateTransactionDetail() *TransactionDetail {
+func (detail *TransactionDetail) CreateTransactionDetail() *TransactionDetail {
 	db.NewRecord(detail)
 	db.Create(&detail)
 	return detail
+}
+
+func CreateTransaction(userId int) TransactionInformation {
+	FoodInCartList := GetFoodFromCart(userId)
+
+	if len(FoodInCartList) == 0 {
+		panic("No Item(s) in Cart")
+	}
+
+	header := TransactionHeader{}
+
+	header.PurchaseStatus = true
+	header.UserId = userId
+	header.CreateTransactionHeader()
+
+	var transactionDetails []TransactionDetail
+
+	for _, food := range FoodInCartList {
+		detail := TransactionDetail{}
+
+		detail.TransactionId = int(header.ID)
+		detail.FoodId = food.FoodId
+		detail.Quantity = food.Quantity
+		detail.CreateTransactionDetail()
+
+		DeleteFoodFromCart(userId, detail.FoodId)
+
+		transactionDetails = append(transactionDetails, detail)
+	}
+
+	transactionInfo := TransactionInformation{}
+	transactionInfo.TrHeader = header
+	transactionInfo.TrDetails = transactionDetails
+
+	return transactionInfo
+}
+
+func GetTransactionsHeaderList(userId int) []TransactionHeader {
+	var transactionList []TransactionHeader
+	db.Where("User_Id=?", userId).Find(&transactionList)
+	return transactionList
 }
 
 func GetTransactionHeader(transactionId int) TransactionHeader {
@@ -53,13 +94,8 @@ func GetTransactionDetails(transactionId int) TransactionInformation {
 	var transactionDetails []TransactionDetail
 	db.Where("Transaction_Id=?", transactionId).Find(&transactionDetails)
 	trnsactionInfo := TransactionInformation{
-		TrHeader: transactionHeader,
+		TrHeader:  transactionHeader,
 		TrDetails: transactionDetails,
 	}
 	return trnsactionInfo
-}
-
-func confirmPurchase(transactionId int) /**TransactionHeader*/ {
-	// header.PurchaseStatus = true
-	println("")
 }
